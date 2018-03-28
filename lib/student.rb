26175b14b814b4a331c00d87dd94f56467e74512
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 00:15:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,15 +1,21 @@
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-  
+
   def initialize(name:, grade:, id: => nil)
     @name = name
     @grade = grade
-    
+
+  end
+
+  def self.create_table
+    sql = <<- SQL
+
+    DB[:conn].execute(sql)
   end
 
   def save
 
   end
-  
+
 end
